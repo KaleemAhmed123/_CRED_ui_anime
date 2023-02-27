7a7a2245a750e87e06408258edc797b059896815
KaleemAhmed123 <shaamidreez@gmail.com>
2023-02-27 09:20:22 +0530

    Security page done.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Experience from "../components/experience/Experience";
 import FeelSpecial from "../components/feelSpecial/FeelSpecial";
+import Security from "../components/security/Security";
 import Header from "../components/header/Header";
 import Hero from "../components/heroSection/Hero";
 import ProductShowcase from "../components/productShowcase/ProductShowcase";
@@ -13,6 +14,7 @@ const HomePage = () => {
       <ProductShowcase />
       <FeelSpecial />
       <Experience />
+      <Security />
     </>
   );
 };
