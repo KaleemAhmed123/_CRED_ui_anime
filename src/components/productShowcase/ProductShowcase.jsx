e7bccc9109257766915b4d3c132943d2e689f3e8
KaleemAhmed123 <shaamidreez@gmail.com>
2023-02-28 01:19:58 +0530

    Final touch done and last commit.

--- src/components/productShowcase/ProductShowcase.jsx
+++ src/components/productShowcase/ProductShowcase.jsx
@@ -40,23 +40,23 @@ const ProductShowcase = () => {
       {showAnimation && (
         <div className="showcase-wrapper">
           <img
-            src="https://web-images.credcdn.in/_next/assets/images/home-page/phone/left-2.png"
+            src="https://web-images.credcdn.in/_next/assets/images/home-page/phone/neopop-left-2.png"
             className="showcase-ui showcase-mockup-1"
           />
           <img
-            src="https://web-images.credcdn.in/_next/assets/images/home-page/phone/left-1.png"
+            src="https://web-images.credcdn.in/_next/assets/images/home-page/phone/neopop-left-1.png"
             className="showcase-ui showcase-mockup-2"
           />
           <img
-            src="https://web-images.credcdn.in/_next/assets/images/home-page/phone/center.png"
+            src="https://web-images.credcdn.in/_next/assets/images/home-page/phone/neopop-center.png"
             className="showcase-ui showcase-mockup-3"
           />
           <img
-            src="https://web-images.credcdn.in/_next/assets/images/home-page/phone/right-1.png"
+            src="https://web-images.credcdn.in/_next/assets/images/home-page/phone/neopop-right-1.png"
             className="showcase-ui showcase-mockup-4"
           />
           <img
-            src="https://web-images.credcdn.in/_next/assets/images/home-page/phone/right-2.png"
+            src="https://web-images.credcdn.in/_next/assets/images/home-page/phone/neopop-right-2.png"
             className="showcase-ui showcase-mockup-5"
           />
         </div>
